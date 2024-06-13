9cbff356283979c09dc1e5f6bcf8754f7354feff
NicoNicoNii11 <nguyenhau.dothi@gmail.com>
2024-06-13 22:35:59 +0700

    done responsive, sidebar

---
+++ admin/src/components/Container.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Container = ({ children }) => {
+  return (
+    <div className="container mx-auto px-4 sm:px-6 md:px-8">
+      {children}
+    </div>
+  );
+};
+
+export default Container;
--- admin/src/pages/User/AddStaff.jsx
+++ admin/src/pages/User/AddStaff.jsx
@@ -18,33 +18,32 @@ const AddStaff = () => {
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <h2 className="mb-8 text-3xl font-bold">Thêm nhân viên</h2>
       <form
-        className="flex lg:w-[1180px] flex-col flex-wrap gap-4"
+        className="flex flex-col gap-4"
         onSubmit={""}
       >
         {/* row1 */}
-        <div className="flex gap-8">
+        <div className="flex flex-col lg:flex-row gap-4">
           <div className="lg:w-1/2">
-            <div className="mb-2 block">
+            <div className="mb-2">
               <Label htmlFor="fullname" value="Họ và tên" />
             </div>
             <InputText
               id="fullname"
               type="text"
-              className=" w-full border border-gray-300 rounded-md"
-              name='fullname'
+              className="w-full border border-gray-300 rounded-md"
+              name="fullname"
               placeholder="Nhập họ và tên"
               onChange={""}
               required />
           </div>
           <div className="lg:w-1/2">
-            <div className="mb-2 block">
-              <Label htmlFor="age" value="Email" />
+            <div className="mb-2">
+              <Label htmlFor="email" value="Email" />
             </div>
             <InputText
-              keyfilter="email"
               id="email"
               type="text"
-              className=" w-full border border-gray-300 rounded-md"
+              className="w-full border border-gray-300 rounded-md"
               name="email"
               placeholder="Nhập email"
               onChange={""}
@@ -53,23 +52,23 @@ const AddStaff = () => {
         </div>
 
         {/* row2 */}
-        <div className="flex gap-8">
+        <div className="flex flex-col lg:flex-row gap-4">
           <div className="lg:w-1/2">
-            <div className="mb-2 block">
+            <div className="mb-2">
               <Label htmlFor="phone" value="Số điện thoại" />
             </div>
             <InputMask
               id="phone"
               mask="999-999-9999"
               placeholder="Nhập số điện thoại"
-              className=" w-full border border-gray-300 rounded-md"
+              className="w-full border border-gray-300 rounded-md"
               name="phone"
               onChange={""}
               required />
           </div>
 
           <div className="lg:w-1/2">
-            <div className="mb-2 block">
+            <div className="mb-2">
               <Label htmlFor="dob" value="Ngày sinh" />
             </div>
             <Calendar
@@ -83,16 +82,15 @@ const AddStaff = () => {
         </div>
 
         {/* row3 */}
-        <div className="flex gap-8">
+        <div className="flex flex-col lg:flex-row gap-4">
           <div className="lg:w-1/2">
-            <div className="mb-2 block">
+            <div className="mb-2">
               <Label htmlFor="province" value="Tỉnh/Thành phố" />
             </div>
-            {/* <Province onProvinceChange={setSelectedProvince} 
-            /> */}
+            {/* <Province onProvinceChange={setSelectedProvince} /> */}
           </div>
           <div className="lg:w-1/2">
-            <div className="mb-2 block">
+            <div className="mb-2">
               <Label htmlFor="district" value="Quận/Huyện" />
             </div>
             {/* {selectedProvince && (
@@ -105,42 +103,40 @@ const AddStaff = () => {
         </div>
 
         {/* row4 */}
-        <div className="flex gap-8">
-        <div className="lg:w-1/2">
-        <div className="mb-2 block">
-          <Label htmlFor="ward" value="Phường/Xã" />
-        </div>
-        {/* {selectedDistrict && (
-          <Ward
-            districtCode={selectedDistrict}
-            onWardChange={setSelectedWard}
-          />
-        )} */}
-      </div>
-
+        <div className="flex flex-col lg:flex-row gap-4">
+          <div className="lg:w-1/2">
+            <div className="mb-2">
+              <Label htmlFor="ward" value="Phường/Xã" />
+            </div>
+            {/* {selectedDistrict && (
+              <Ward
+                districtCode={selectedDistrict}
+                onWardChange={setSelectedWard}
+              />
+            )} */}
+          </div>
 
           <div className="lg:w-1/2">
-            <div className="mb-2 block">
+            <div className="mb-2">
               <Label htmlFor="address" value="Địa chỉ cụ thể" />
             </div>
             <InputText
               id="address"
               type="text"
-              className=" w-full border border-gray-300 rounded-md"
-              name='address'
+              className="w-full border border-gray-300 rounded-md"
+              name="address"
               placeholder="Nhập địa chỉ cụ thể"
               onChange={""}
               required 
-              style={{height: '60%'}}/>
+              style={{ height: '60%' }}/>
           </div>
-
         </div>
 
-        <Button type="submit" className="mt-5">
+        <Button type="submit" className="mt-5 bg-teal-600 text-white py-2 px-4 rounded-md hover:bg-teal-700">
           Thêm nhân viên
         </Button>
-      </form >
-    </div >
+      </form>
+    </div>
   )
 }
 
