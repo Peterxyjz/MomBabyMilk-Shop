b2bdc29045ffe7e6aef1873fabc32b2e471840b9
XuanThinhTu <tuthinh2004@gmail.com>
2024-06-17 19:17:31 +0700

    FilterPage, News, Update Card

--- user/src/pages/Home/index.jsx
+++ user/src/pages/Home/index.jsx
@@ -5,6 +5,42 @@ import SecondHero from "../../components/hero/SecondHero";
 import CategoryGrid from "../../components/card/CategorGrid";
 import HeroAtLast from "../../components/hero/HeroAtLast";
 import ProductCard from "../../components/card/Card";
+import { useProductContext } from "../../context/ProductContext";
+import Loader from "../../assets/loading.gif";
+import NewsSection from "../../components/news/NewsSection";
+
+const newsData = [
+  {
+    id: 1,
+    imageUrl: '/path/to/image1.jpg',
+    date: '17',
+    month: 'T06',
+    title: 'Vitamin D3 và lợi ích không thể thiếu cho bé yêu nhà bạn',
+    excerpt: 'Vitamin D3 cho trẻ là một sản phẩm không thể thiếu...',
+    comments: 0,
+    author: 'Nhi Marketing',
+  },
+  {
+    id: 2,
+    imageUrl: '/path/to/image2.jpg',
+    date: '17',
+    month: 'T06',
+    title: 'Hướng dẫn chăm sóc não bộ ở trẻ nhỏ',
+    excerpt: 'Chăm sóc não bộ cho trẻ nhỏ là một việc hết sức quan trọng...',
+    comments: 0,
+    author: 'Nhi Marketing',
+  },
+  {
+    id: 3,
+    imageUrl: '/path/to/image3.jpg',
+    date: '01',
+    month: 'T06',
+    title: '[Hà Nội] Thế Giới Sữa tuyển dụng',
+    excerpt: 'VIỆC NGAY TẦM TAY – CHỚP NGAY KẺO LỠ...',
+    comments: 0,
+    author: 'ADMIN THẾ GIỚI SỮA',
+  },
+];
 
 const MomNBaby = {
   title: "Sản phẩm hỗ trợ cho mẹ và bé",
@@ -91,19 +127,27 @@ const Mom = {
 };
 
 const Home = () => {
+  const { products, loading } = useProductContext();
+  if (loading)
+    return (
+      <div
+        className="fixed w-full h-full z-[10000] flex items-center justify-center bg-white"
+        style={{ left: 0, top: 0 }}
+      >
+        <img src={Loader} alt="Loading..." />
+      </div>
+    );
   return (
     <div className="min-h-screen">
       <Hero />
-
-      <ProductCard />
-      <div>
-        <Slider title={MomNBaby.title} slides={MomNBaby.slides} />
-        <Slider title={Mom.title} slides={Mom.slides} />
-      </div>
+      <ProductCard products={products} headline={"Sản phẩm mới"} />
+      <ProductCard products={products} headline={"Sản phẩm bán chạy"} />
       <SecondHero />
-      <ProductCard />
+      <ProductCard products={products} headline={"Sữa dành cho mẹ bầu"} />
+      <ProductCard products={products} headline={"Sữa dành cho trẻ sơ sinh"} />
       <HeroAtLast />
       <CategoryGrid />
+      <NewsSection news={newsData} />
     </div>
   );
 };
