6850c8ad1bc550b82323ad978c68b08dc76c5eb5
Peterxyjz <huyq09@gmail.com>
2024-06-27 00:44:38 +0700

    layout changepassword

--- user/src/components/profile/SideBarProfile.jsx
+++ user/src/components/profile/SideBarProfile.jsx
@@ -3,32 +3,61 @@
 import { Sidebar } from "flowbite-react";
 import {
   HiChat,
-  HiClock ,
+  HiClock,
   HiUser,
   HiGift,
   HiOutlineAdjustments  
 } from "react-icons/hi";
+import { useLocation } from "react-router-dom";
 
 export function SideBarProfile() {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  const getItemClass = (path) => 
+    currentPath === path 
+      ? "bg-blue-500 text-white font-bold text-lg px-6 py-3 "
+      : "text-gray-600 hover:bg-gray-100 text-lg px-6 py-3 ";
+
   return (
-    <Sidebar className="bg-white border mx-4">
+    <Sidebar className="bg-white border mx-4 rounded-xl">
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-          <Sidebar.Item href="/profile" icon={HiUser}>
+          <Sidebar.Item 
+            href="/profile" 
+            icon={HiUser} 
+            className={getItemClass("/profile")}
+          >
             Tài khoản
           </Sidebar.Item>
-          <Sidebar.Item href="/profile/change-password" icon={HiOutlineAdjustments}>
+          <Sidebar.Item 
+            href="/profile/change-password" 
+            icon={HiOutlineAdjustments} 
+            className={getItemClass("/profile/change-password")}
+          >
             Mật khẩu & bảo mật
           </Sidebar.Item>
         </Sidebar.ItemGroup>
         <Sidebar.ItemGroup>
-          <Sidebar.Item href="/profile/accumulated-points" icon={HiGift}>
-            Đổi quà
+          <Sidebar.Item 
+            href="/profile/accumulated-points" 
+            icon={HiGift} 
+            className={getItemClass("/profile/accumulated-points")}
+          >
+            Đổi quà
           </Sidebar.Item>
-          <Sidebar.Item href="/profile/history-order" icon={HiClock }>
+          <Sidebar.Item 
+            href="/profile/history-order" 
+            icon={HiClock} 
+            className={getItemClass("/profile/history-order")}
+          >
             Lịch sử đơn hàng
           </Sidebar.Item>
-          <Sidebar.Item href="/profile/my-feedback" icon={HiChat  }>
+          <Sidebar.Item 
+            href="/profile/my-feedback" 
+            icon={HiChat} 
+            className={getItemClass("/profile/my-feedback")}
+          >
             Bình luận của tôi
           </Sidebar.Item>
         </Sidebar.ItemGroup>
--- user/src/pages/Profile/index.jsx
+++ user/src/pages/Profile/index.jsx
@@ -8,11 +8,11 @@ const Profile = () => {
     <div>
       <Breadcrumbs headline="Thông tin tài khoản" />
       <div className="bg-[#f5f5f5] py-4">
-        <div className="container mx-auto flex gap-4 my-4 mx-4">
-          <div className="w-1/5">
+        <div className="container flex gap-4 my-4 mx-4">
+          <div className="w-1/5 ">
             <SideBarProfile />
           </div>
-          <div className="w-4/5 bg-white border p-8 mx-4">
+          <div className="w-4/5 bg-white border p-10 mx-10 rounded-xl">
             <Outlet />
           </div>
         </div>
