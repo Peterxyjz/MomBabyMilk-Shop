7da3dbea5e713e3e4b28eb10ab15096ec71495b2
Peterxyjz <huyq09@gmail.com>
2024-07-03 19:59:48 +0700

    done voucher - fix member_Ship

--- user/src/components/order/Thanks.jsx
+++ user/src/components/order/Thanks.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useLocation } from "react-router-dom";
 import successImg from "../../assets/images/background/order-success-poster.png";
 import { IoMdCloseCircleOutline } from "react-icons/io";
