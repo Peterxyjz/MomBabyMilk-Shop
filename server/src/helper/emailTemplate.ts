eb0971ae37d3a034fdf79c464587a845050e9e50
phongnqse172516@gmail.com <phongnqse172516@gmail.com>
2024-07-06 18:47:38 +0700

    upload send email when banned or unblocked

--- server/src/helper/emailTemplate.ts
+++ server/src/helper/emailTemplate.ts
@@ -316,3 +316,102 @@ export function generateInvoiceHTML(order: any, orderDetail: any) {
     </html>
     `
 }
+
+export function generateEmailStatus(username: string, status: string): string {
+    return `
+  <!DOCTYPE html>
+  <html>
+  
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Email Verification</title>
+      <style>
+          body {
+              font-family: Arial, sans-serif;
+              background-color: #f4f4f4;
+              margin: 0;
+              padding: 0;
+          }
+  
+          .container {
+              background-color: #f9f9f9;
+              max-width: 600px;
+              margin: 0 auto;
+              background-color: #fff;
+              padding: 20px;
+              border-radius: 5px;
+              box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+          }
+  
+          .header {
+              padding: 10px 0;
+          }
+  
+          .header h1 {
+              text-align: center;
+              color: #333333;
+              font-size: 24px;
+          }
+  
+          .content {
+              font-size: 16px;
+              padding: 20px;
+              border-radius: 5px;
+          }
+  
+          .content p {
+              color: #666666;
+              line-height: 1.6;
+          }
+          .button-container {
+              align-items: center;
+              padding: 20px 0;
+          }
+  
+          .button-link {
+              display: inline-block;
+              padding: 10px 50px;
+              font-size: 16px;
+              color: #fff;
+              background-color: #007bff;
+              text-decoration: none;
+              border-radius: 5px;
+              font-weight: 500;
+              text-align: center;
+              color: inherit;
+              cursor: default;
+          }
+  
+          .footer {
+              padding: 10px;
+              font-size: 14px;
+              color: #999999;
+              text-align: center;
+          }
+      </style>
+  </head>
+  
+  <body>
+      <div class="container">
+          <div class="header">
+              <h1>Trạng thái tài khoản</h1>
+          </div>
+          <div class="content">
+              <p>Xin Chào ${username},</p>
+              <p>Cảm ơn bạn đã sử dụng dịch vụ của chúng tôi. 
+               Hiện tại tài khoản của bạn đã ${status}
+              </p>
+             
+              <p>Chúng tôi rất xin lỗi vì sự việc này, hãy liên hệ với chúng tôi ngay để biết thêm chi tiết.</p>
+              <p>Trân Trọng,<br>MomBabyMilk Shop</p>
+          </div>
+          <div class="footer">
+              <p>&copy; 2024 MomBabyMilk Shop. All rights reserved.</p>
+          </div>
+      </div>
+  </body>
+  
+  </html>
+  `
+  }
