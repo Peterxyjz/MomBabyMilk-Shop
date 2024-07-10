ac53cf6233a3445144442d8da681748b94b5200c
NicoNicoNii11 <nguyenhau.dothi@gmail.com>
2024-07-11 00:40:03 +0700

    fix update voucher

--- admin/src/pages/Voucher/Vouchers.jsx
+++ admin/src/pages/Voucher/Vouchers.jsx
@@ -244,6 +244,177 @@ const Vouchers = () => {
             className="w-full text-sm text-left rtl:text-right text-gray-500"
           />
         </Card>
+
+        {showModal && selectedVoucher && (
+          <div
+            id="editUserModal"
+            tabIndex="-1"
+            aria-hidden="true"
+            className="fixed top-0 left-0 right-0 z-50 items-center justify-center w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full flex"
+          >
+            <div className="relative w-full max-w-2xl max-h-full">
+              {/* Modal content */}
+              <form className="relative bg-white rounded-lg shadow " onSubmit={handleSubmit}>
+                {/* Modal header */}
+                <div className="flex items-start justify-between p-4 border-b rounded-t ">
+                  <h3 className="text-xl font-semibold text-gray-900 ">
+                    Chỉnh sửa mã voucher
+                  </h3>
+                  <button
+                    type="button"
+                    className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center "
+                    onClick={toggleModal}
+                  >
+                    <svg
+                      className="w-3 h-3"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 14 14"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                      />
+                    </svg>
+                    <span className="sr-only">Close modal</span>
+                  </button>
+                </div>
+                {/* Modal body */}
+                <div className="p-6 space-y-6">
+                  <div className="grid grid-cols-6 gap-6">
+                    <div className="col-span-6 sm:col-span-3">
+                      <label
+                        htmlFor="category"
+                        className="block mb-2 text-sm font-medium text-gray-900 "
+                      >
+                        Loại Voucher
+                      </label>
+                      <Select
+                        id="category"
+                        className="w-full rounded"
+                        onChange={(e) =>
+                          setSelectedVoucher({
+                            ...selectedVoucher,
+                            voucher_type: e.target.value,
+                          })
+                        }
+                        value={selectedVoucher.voucher_type}
+                        required
+                      >
+                        <option value="" disabled>
+                          Chọn Loại Voucher
+                        </option>
+                        {voucherTypes.map((option) => (
+                          <option key={option.id} value={option.id}>
+                            {option.name}
+                          </option>
+                        ))}
+                      </Select>
+                    </div>
+                    <div className="col-span-6 sm:col-span-3">
+                      <label
+                        htmlFor="amount"
+                        className="block mb-2 text-sm font-medium text-gray-900 "
+                      >
+                        Số lượng
+                      </label>
+                      <TextInput
+                        id="amount"
+                        type="number"
+                        min={0}
+                        value={selectedVoucher.amount}
+                        name="amount"
+                        placeholder="Số lượng..."
+                        onChange={(e) =>
+                          setSelectedVoucher({
+                            ...selectedVoucher,
+                            amount: e.target.value,
+                          })
+                        }
+                        required
+                      />
+                    </div>
+                    <div className="col-span-6 sm:col-span-3">
+                      <label
+                        htmlFor="memberShip"
+                        className="block mb-2 text-sm font-medium text-gray-900 "
+                      >
+                        MemberShip (Đổi bằng bao nhiêu điểm)
+                      </label>
+                      <TextInput
+                        id="memberShip"
+                        type="number"
+                        name="memberShip"
+                        placeholder="MemberShip..."
+                        min={0}
+                        value={selectedVoucher.membership}
+                        onChange={(e) =>
+                          setSelectedVoucher({
+                            ...selectedVoucher,
+                            membership: e.target.value,
+                          })
+                        }
+                        required
+                      />
+                    </div>
+                    <div className="col-span-6 sm:col-span-3">
+                      <label
+                        htmlFor="discount"
+                        className="block mb-2 text-sm font-medium text-gray-900 "
+                      >
+                        Mức giảm giá (VND)
+                      </label>
+                      <TextInput
+                        id="discount"
+                        type="number"
+                        min={0}
+                        name="discount"
+                        value={selectedVoucher.discount}
+                        placeholder="Mức giảm giá..."
+                        onChange={(e) =>
+                          setSelectedVoucher({
+                            ...selectedVoucher,
+                            discount: e.target.value,
+                          })
+                        }
+                        required
+                      />
+                    </div>
+                    <div className="col-span-6 sm:col-span-3">
+                      <label
+                        htmlFor="expire_date"
+                        className="block mb-2 text-sm font-medium text-gray-900 "
+                      >
+                        Ngày hết hạn
+                      </label>
+                      <Datepicker
+                        language="vi"
+                        defaultDate={new Date(selectedVoucher.expire_date)}
+                        onSelectedDateChanged={(date) =>
+                          setSelectedVoucher({
+                            ...selectedVoucher,
+                            expire_date: date,
+                          })
+                        }
+                        required
+                      />
+                    </div>
+                  </div>
+                </div>
+                {/* Modal footer */}
+                <div className="flex items-center p-6 space-x-3 rtl:space-x-reverse border-t border-gray-200 rounded-b ">
+                  <Button type="submit" className="text-white bg-blue-700 hover:bg-blue-800">
+                    Lưu thay đổi
+                  </Button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
