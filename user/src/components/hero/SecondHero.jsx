d8cc33d93b8c4c11df597e960e43e32189477064
XuanThinhTu <tuthinh2004@gmail.com>
2024-07-10 14:05:48 +0700

    edit banner for HomePage

--- user/src/components/hero/SecondHero.jsx
+++ user/src/components/hero/SecondHero.jsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import baby_drink_milk from '../../assets/images/body/babyDrinkMilk.png';
+import banner from "../../assets/images/background/banner_baby.png"
 
 function SecondHero() {
     return (
-        <div className="py-20 mt-12">
-            <div className="container mx-auto px-6 md:px-12">
-                <div className="flex flex-col md:flex-row items-center"
-                    style={{
-                        backgroundImage: `url(${baby_drink_milk})`,
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        padding: '100px',
-                        borderRadius: '10px'
-                    }}>
-
-                    <div className="md:w-1/2 lg:w-2/3">
-                        <h1 className="text-4xl md:text-6xl lg:text-7xl text-black font-bold mb-6">
-                            <br className="hidden md:block" />
-                            <span className="text-indigo-500">MomBabyMilk</span> Website
-                        </h1>
-
-                    </div>
+        <div className="relative bg-cover bg-center mt-12" style={{ backgroundImage: `url(${banner})`, height: '500px' }}>
+            <div className="absolute inset-0 flex items-center justify-start bg-opacity-50">
+                <div className="md:w-1/2 lg:w-2/3 ml-10 p-6">
+                    <h1 className="text-4xl md:text-6xl lg:text-7xl text-black font-bold mb-6">
+                        Sữa dành cho <br className="hidden md:block" />
+                        <span className="text-indigo-500">Mẹ và Bé</span>
+                    </h1>
                 </div>
             </div>
         </div>
